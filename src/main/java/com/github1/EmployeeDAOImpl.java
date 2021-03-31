d50262b5c23b33398e8d6ccf71f861410a1ee302
Lenovo <Lenovo@Lenovo-PC.Dlink>
2021-03-31 20:49:19 +0530

    Pl review my code

---
+++ src/main/java/com/github1/EmployeeDAOImpl.java
@@ -0,0 +1,27 @@
+package com.github1;
+
+import java.util.List;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	public void saveEmployee(Employee emp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateEmployee(int empId, int empSalary) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteEmployee(int empId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Employee> listEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
